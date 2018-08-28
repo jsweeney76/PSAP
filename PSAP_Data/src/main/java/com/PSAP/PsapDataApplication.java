72339237ffa12e122102e9576d0b254cd9e10482
jimsweeney <jimsweeney@10.0.0.88>
2018-08-27 22:39:02 -0400

    Initial check-in for PSAP App

---
+++ PSAP_Data/src/main/java/com/PSAP/PsapDataApplication.java
@@ -0,0 +1,12 @@
+package com.PSAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsapDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsapDataApplication.class, args);
+	}
+}
